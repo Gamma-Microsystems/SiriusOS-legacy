45337b03aa33aa20b7c904a5839ae1eeb7f66718
gamma63 <webe-not-inc@proton.me>
2024-07-04 17:03:16 +0300

    Update 0.04
    Add pcinf command
    Started work on VBE
    switch from bin to elf
    switch from -O1 compilation flag to -O3

--- sirius/Kernel.cs
+++ sirius/Kernel.cs
@@ -10,14 +10,14 @@ namespace sirius
 
         protected override void BeforeRun()
         {
-            Console.WriteLine("Welcome to SiriusOS!");
+            Console.WriteLine("\nWelcome to SiriusOS!\n");
         }
         
         protected override void Run()
         {
             Console.Write("$ ");
             var builtin = Console.ReadLine();
-            var OS_VER = "1.0.0 Milestone 1, 0.03";
+            var OS_VER = "1.0.0 Milestone 1, 0.04";
             switch (builtin) {
                 default:
                     Console.WriteLine(builtin + ": Is not a valid command");
@@ -29,12 +29,25 @@ namespace sirius
                     Console.WriteLine("ver - display version");
                     Console.WriteLine("poweroff - turn off the computer");
                     Console.WriteLine("reboot - reboot the computer");
+                    Console.WriteLine("pcinf - shows the computer information");
                     break;
                 case "poweroff":
                     Cosmos.System.Power.Shutdown();
                     break;
                 case "reboot":
                     Cosmos.System.Power.Reboot();
+                    break;
+                case "pcinf":
+                    string CPUBRAND = Cosmos.Core.CPU.GetCPUBrandString();
+                    string CPUVENDOR = Cosmos.Core.CPU.GetCPUVendorName();
+                    uint RAMSIZE = Cosmos.Core.CPU.GetAmountOfRAM();
+                    ulong FREERAM = Cosmos.Core.GCImplementation.GetAvailableRAM();
+                    uint RAMUSED = Cosmos.Core.GCImplementation.GetUsedRAM();
+                    Console.WriteLine(@"CPU: {0}
+                                        CPU Vendor: {1}
+                                        Amount of RAM: {2}
+                                        Available RAM: {3}
+                                        Used RAM: {4}",CPUBRAND,CPUVENDOR,RAMSIZE,FREERAM,RAMUSED);
                     break;
             }
         }
