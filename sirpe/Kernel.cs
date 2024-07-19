8a192517fed89d2e887784d300ba25b3da9a470c
gamma63 <webe-not-inc@proton.me>
2024-07-19 19:30:32 +0300

    SirPE build 5

--- sirpe/Kernel.cs
+++ sirpe/Kernel.cs
@@ -2,16 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
-using SimpleFileSystem;
 
 namespace sirpe
 {
     public class Kernel: Sys.Kernel
     {
+        Sys.FileSystem.CosmosVFS fs;
+        string current_directory = "0:\\";
 
         protected override void BeforeRun()
         {
+            fs = new Sys.FileSystem.CosmosVFS();
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
             Console.Clear();
+            Console.WriteLine("SiriusOS Milestone 3 Setup\n\n");
+            Console.WriteLine("Welcome to SiriusOS Milestone 3 setup!");
         }
         
         protected override void Run()
