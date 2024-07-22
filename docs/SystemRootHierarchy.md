a2106422b5fa33585d64643cb1cded95c2a8b37e
gamma63 <174414990+gamma63@users.noreply.github.com>
2024-07-22 12:43:23 +0300

    SirPE build 16

---
+++ docs/SystemRootHierarchy.md
@@ -0,0 +1,12 @@
+# System Root Hiearchy in SiriusOS
+
+| Directory | Purpose |
+|-----------|---------|
+| Cache     | Storing cache  |
+| Geek      | Storing files for advanced users |
+| Libraries | Stroing libraries |
+| Media     | Storing removable devices data |
+| Programs  | Stroing programs |
+| Protected | Stroring software which runs on boot (this directory must be read only when SiriusOS is loaded) |
+| Temporary | Storing temporary files |
+| Unsafe    | Nonfree software |
--- sirpe/Kernel.cs
+++ sirpe/Kernel.cs
@@ -18,9 +18,88 @@ namespace sirpe
             Console.Clear();
             Console.WriteLine("SiriusOS Milestone 3 Setup\n\n");
             Console.WriteLine("Welcome to SiriusOS Milestone 3 setup!");
-            //Console.WriteLine("\nPress enter to contiune");
+            Console.WriteLine("\nPress enter to contiune or escape to exit setup");
+            try
+            {
+                while (true)
+                {
+                    var key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            Cosmos.System.Power.Shutdown();
+                            break;
+                        case ConsoleKey.Enter:
+                            Agreement();
+                            break;
+                        default:
+                            break;
+                    }
+                    Cosmos.Core.Memory.Heap.Collect();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Fatal: Double fault.");
+            }
         }
-        
+
+        public static void Agreement()
+        {
+            Console.Clear();
+            Console.WriteLine("This Product is licensed under GNU General Public License 3.\nBy installing this product you agree with this license.\n<https://raw.githubusercontent.com/gamma63/SiriusOS/pc/LICENSE>");
+            Console.WriteLine("\n\n\nPress Y to contiune or N to exit setup");
+            try
+            {
+                while (true)
+                {
+                    var key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.N:
+                            Cosmos.System.Power.Shutdown();
+                            break;
+                        case ConsoleKey.Y:
+                            UnderConstruction();
+                            break;
+                        default:
+                            break;
+                    }
+                    Cosmos.Core.Memory.Heap.Collect();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Fatal: Double fault.");
+            }
+        }
+
+        public static void UnderConstruction()
+        {
+            Console.Clear();
+            Console.WriteLine("Oops...\nSeems like this part is not yet implemented.\nPress escape to exit.");
+            try
+            {
+                while (true)
+                {
+                    var key = Console.ReadKey(true);
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.Escape:
+                            Cosmos.System.Power.Shutdown();
+                            break;
+                        default:
+                            break;
+                    }
+                    Cosmos.Core.Memory.Heap.Collect();
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Fatal: Double fault.");
+            }
+        }
+
         protected override void Run()
         {
         }
